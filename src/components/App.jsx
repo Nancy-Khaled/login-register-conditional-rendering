b76bc43814555ed64d26c772ad4d7164a1c7f8ce
Nancy-Khaled <131464596+Nancy-Khaled@users.noreply.github.com>
2023-05-30 18:29:17 +0300

    login-register with use state hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Form";
 
-var userIsRegistered = false;
-
 function App() {
+
+  const [startState, setValue] = useState(true);
+
+  function Login() {
+    setValue(true)
+  }
+
+  function Register() {
+    setValue(false)
+  }
+
   return (
-    <div className="container">
-      <div className="row justify-content-center align-items-center">
-        <Form isRegistered={userIsRegistered} />
+    <div>
+      <header>
+        <button type="button" className="btn btn-secondary" onClick={Login}>Login</button>
+        <button type="button" className="btn btn-outline-secondary" onClick={Register}>Register</button>
+      </header>
+
+      <div className="container">
+        <div className="row justify-content-center align-items-center">
+          <Form isRegistered={startState} />
+        </div>
       </div>
     </div>
   );

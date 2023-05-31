b65bf73c2fdbae6911e6b50689696c7addb25380
Nancy-Khaled <131464596+Nancy-Khaled@users.noreply.github.com>
2023-05-31 12:25:28 +0300

    login-register event & forms update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form(props) {
+
+    const [name, setName] = useState("")
+    const [textName, setTextName] = useState("")
+
+    function handleChange(e) {
+        setName(e.target.value)
+    }
+
+    function handleClick(eve) {
+        setTextName(name)
+        
+        eve.preventDefault()
+    }
+
     return (
         <div className="col-md-6">
-            <form className="form">
+
+        <h4 className="text-center">{textName}</h4>
+
+            <form className="form" onSubmit={handleClick}>
                 <h2 className="text-center mb-3">{props.isRegistered ? "Login" : "Register"}</h2>
                 <div className="form-group">
                     <label htmlFor="username">User name:</label><br />
-                    <input type="text" placeholder="Username" id="username" className="form-control" />
+                    <input type="text" onChange={handleChange} value={name} placeholder="Username" id="username" className="form-control" />
                 </div>
 
                 <div className="form-group">
@@ -21,7 +38,7 @@ function Form(props) {
                     </div>
                 )}
 
-                <button className="mt-2" type="submit">{props.isRegistered ? "Login" : "Register"}</button>
+                <button className="mt-2" type="submit">{props.isRegistered ? " Login" : "Register"}</button>
             </form>
         </div>
     );
